30242479444ab90ffb11606ac15fd92443a30583
Elison Marcio Correa <marcioinfo.correa@gmail.com>
2019-05-02 00:06:06 -0700

    Add Auth0 facebook and google provider

--- vagrant/catalog/app.py
+++ vagrant/catalog/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash, abort, g
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from catalogModel import Base, Catalog, CatalogItem, User
@@ -7,8 +7,6 @@ from flask import session as user_session
 from flask import make_response
 import random
 import string
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.client import FlowExchangeError
 import httplib2
 import json
 import requests
@@ -16,6 +14,9 @@ import google.oauth2.credentials
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import os
+from flask_httpauth import HTTPBasicAuth
+auth = HTTPBasicAuth()
+from flask_mail import Message, Mail
 
 
 SCOPES="profile openid email"
@@ -52,6 +53,27 @@ with open(CLIENT_SECRETS_FILE, 'r') as f:
 
 APPLICATION_NAME = "Category Store"
 
+checkpass =[];
+@app.route("/singapp", methods=['GET', 'POST'])
+def singin():
+    if request.method == 'POST':
+        checkpass = request.form['password']
+        checkuser = request.form['email']
+
+        if checkpass is None or checkuser is None:
+            abort(401)  # missign argument
+
+        if session.query(User).filter_by(email=checkuser).first() is not None:
+            abort(400)  # Existing User
+
+        newUser = User(name=request.form['name'], email=request.form['email'], password_hash=request.form['password'])
+        session.add(newUser)
+        session.commit()
+        user = session.query(User).filter_by(email=user_session['email']).one()
+        login_session['name'] = user.name
+        return render_template('newuser.html')
+    else:
+        return render_template('newuser.html')
 
 
 
@@ -84,7 +106,7 @@ def fbconnect():
         and replace the remaining quotes with nothing so that it can be used directly in the graph
         api calls
     '''
-    token = result.split(',')[0].split(':')[1].replace('"', '')
+    token = result.split(',')[0].split(':')[1].replace('"', '').decode('utf-8')
 
     url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name,id,email' % token
     h = httplib2.Http()
@@ -125,6 +147,7 @@ def fbconnect():
 
     flash("Now logged in as %s" % login_session['username'])
     return output
+
 
 
 @app.route('/fbdisconnect')
@@ -210,7 +233,6 @@ def oauth2callback():
 
   flash("you are now logged in as %s" % user_session['username'])
 
-  msg = 'Authentication verified for user with email ::  {} and name :: {}'.format(ans['email'], ans['name'])
   return redirect(url_for('showCategories'))
 
 
@@ -267,6 +289,109 @@ def revoke():
     return('An error occurred')
 
 
+@auth.verify_password
+def verify_password(email_or_token, *password):
+    user_id = User.verify_auth_token(email_or_token)
+    if user_id:
+        user = session.query(User).filter_by(id=user_id).one()
+    else:
+        user = session.query(User).filter_by(email=email_or_token).first()
+    if not user or not user.verify_password(password):
+        return False
+    g.user = user
+    return True
+
+
+def send_reset_email(user):
+    token = user.generate_auth_token()
+    msg = Message('Password Reset Request',
+                  sender='noreply@demo.com',
+                  recipients=[user.email])
+    msg.body = f"""To reset your password, visit the following link:{url_for('reset_token', token=token, _external=True)} If you did not make this request then simply ignore this email and no changes will be made."""
+    Mail.send(msg)
+
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if request.method == 'POST':
+       email = request.form['email']
+
+    if email:
+        user = session.query(User).filter_by(email=email).first()
+        send_reset_email(user)
+        flash('An email has been sent with instructions to reset your password.', 'info')
+        return redirect(url_for('login.html'))
+    return render_template('login.html')
+
+
+
+
+@app.route('/reset_password/<int:token>', methods=['GET', 'POST'])
+def reset_token(token):
+    if request.method == 'POST':
+        if 'username' in login_session:
+            return redirect(url_for('showCategories'))
+        else:
+            user_id = verify_password(token)
+            if user_id is None:
+                flash('That is an invalid or expired token', 'warning')
+                return redirect(url_for('reset_request'))
+
+            if request.form['password']:
+                user = getUserInfo(user_id)
+                user.password_hash = User.hash_password(request.form['password'])
+                session.add(user)
+                session.commit()
+                return redirect(url_for('singin'))
+    else:
+        render_template('reset_password.html', token=token)
+
+
+@app.route('/applogin', methods=['GET', 'POST'])
+def applogin():
+    erro = ''
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        if email is None or password is None:
+            erro = 'missign argument'
+        data = session.query(User).filter_by(email=email).first()
+        if data:
+            erro = 'Existing User'
+        if verify_password(email, password):
+            login_session['provider'] = "APP"
+            login_session['username'] = data.name
+            flash("you are now logged in as %s" % login_session['username'])
+            return redirect(url_for('showCategories'))
+        else:
+            erro = 'Incorrect Password try again'
+            return erro
+    else:
+        return render_template('login.html')
+
+
+@app.route("/usersingin", methods=['GET', 'POST'])
+def newUser():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['user-email']
+        password = request.form['user-pass']
+        if email is None or password is None:
+            abort(401) # missign argument
+        if session.query(User).filter_by(email=email).first() is not None:
+            abort(400) # Existing User
+        user = User(email=email, name=name)
+        user.hash_password(password)
+        session.add(user)
+        session.commit()
+        flash("you are now Registered")
+        return redirect(url_for('applogin'))
+    else:
+        return render_template('login.html')
+
+
+
+
 @app.route('/disconnect')
 def disconnect():
     if 'provider' in login_session:
@@ -277,10 +402,13 @@ def disconnect():
             del user_session['picture']
             del user_session['user_id']
             del user_session['provider']
-        flash("You have successfully been logged out.")
         if login_session['provider'] == 'facebook':
             fbdisconnect()
             del login_session['facebook_id']
+        if login_session['provider'] == 'APP':
+            del user_session['username']
+            del user_session['provider']
+            return redirect(url_for('showCategories'))
         del login_session['username']
         del login_session['email']
         del login_session['picture']
@@ -288,6 +416,9 @@ def disconnect():
         del login_session['provider']
         flash("You have successfully been logged out.")
         return redirect(url_for('showCategories'))
+
+
+
     else:
         flash("You were not logged in")
         return ("You were not logged in")
